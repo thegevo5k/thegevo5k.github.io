49c32f9744feaad2d752bc1c9b5eaf5773a26839
thegevo5k <danaguitar2013@gmail.com>
2026-06-24 04:45:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let allDownloads = [];
 let searchTerm = '';
 
 // July 4, 2026, 12:00 AM EDT (UTC-4)
-const UNLOCK_TIME = new Date('2026-07-04T00:00:00-04:00').getTime();
+const UNLOCK_TIME = new Date('2026-06-24T00:05:47-04:00').getTime();
 
 function initCountdown() {
   const overlay = document.getElementById('countdown-overlay');
